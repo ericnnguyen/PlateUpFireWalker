028a44601e7c0a0891833ebbe0922753271d30ed
Nghia Nguyen <neanguyen@ucdavis.edu>
2023-05-15 00:22:54 -0700

    Add missing files

---
+++ Components/CBurningMess.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+using Unity.Entities;
+
+namespace KitchenFireWalker
+{
+    [StructLayout(LayoutKind.Sequential, Size = 1)]
+    public struct CBurningMess : IComponentData
+    {
+    }
+}
---
+++ Systems/BurningMessSystem.cs
@@ -0,0 +1,46 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenLib.References;
+using KitchenMods;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace KitchenFireWalker
+{
+    public class BurningMessSystem : DetermineShoeBonus
+    {
+        EntityQuery Players;
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+            Players = GetEntityQuery(new QueryHelper()
+                .All(typeof(CPlayer), typeof(CPlayerCosmetics), typeof(CPosition)));
+        }
+
+        protected override void OnUpdate()
+        {
+            using NativeArray<CPosition> playerPositions = Players.ToComponentDataArray<CPosition>(Allocator.Temp);
+            using NativeArray<CPlayerCosmetics> playerComestics = Players.ToComponentDataArray<CPlayerCosmetics>(Allocator.Temp);
+            for (int i = 0; i < playerPositions.Length; i++)
+            {
+                CPlayerCosmetics cosmetics = playerComestics[i];
+                if (cosmetics.Shoe != (PlayerShoe)904) continue;
+
+                CPosition position = playerPositions[i];
+                if (GetOccupant(position, OccupancyLayer.Default) != default || GetOccupant(position, OccupancyLayer.Floor) != default) continue;
+
+                Entity newMess = EntityManager.CreateEntity();
+                Set(newMess, new CPosition(position.Position.Rounded()));
+                Set(newMess, default(CMess));
+                Set(newMess, default(CIsOnFire));
+                Set(newMess, default(CBurningMess));
+                Set(newMess, new CCreateAppliance()
+                {
+                    ID = ApplianceReferences.MessKitchen1,
+                    ForceLayer = OccupancyLayer.Floor
+                });
+            }
+        }
+    }
+}
