12e776d14bd44691ecf4a94891baa8f6f45d91c7
Nghia Nguyen <neanguyen@ucdavis.edu>
2023-05-15 21:42:31 -0700

    Increase movement speed on burning mess

--- Systems/BurningMessSystem.cs
+++ Systems/BurningMessSystem.cs
@@ -1,13 +1,12 @@
 ﻿using Kitchen;
 using KitchenData;
 using KitchenLib.References;
-using KitchenMods;
 using Unity.Collections;
 using Unity.Entities;
 
 namespace KitchenFireWalker
 {
-    public class BurningMessSystem : DetermineShoeBonus
+    public class BurningMessSystem : DaySystem
     {
         EntityQuery Players;
 
@@ -24,6 +23,9 @@ namespace KitchenFireWalker
             using NativeArray<CPlayerCosmetics> playerComestics = Players.ToComponentDataArray<CPlayerCosmetics>(Allocator.Temp);
             for (int i = 0; i < playerPositions.Length; i++)
             {
+                //Lower % of fire?
+                //if (Random.Range(0f, 1f) < .9) return;
+                    
                 CPlayerCosmetics cosmetics = playerComestics[i];
                 if (cosmetics.Shoe != (PlayerShoe)904) continue;
 
--- Systems/FireWalkerSystem.cs
+++ Systems/FireWalkerSystem.cs
@@ -1,29 +1,54 @@
 ﻿using Kitchen;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using KitchenData;
+using Unity.Collections;
 using Unity.Entities;
+using UnityEngine;
 
 namespace KitchenFireWalker
 {
-    internal class FireWalkerSystem : DaySystem
+    [UpdateAfter(typeof(DetermineShoeBonus))]
+    [UpdateBefore(typeof(DeterminePlayerSpeed))]
+    internal class FireWalkerSystem : GenericSystemBase
     {
         EntityQuery Players;
+        EntityQuery Slowers;
 
         protected override void Initialise()
         {
             base.Initialise();
-            /*
             Players = GetEntityQuery(new QueryHelper()
-                .All(typeof(CPlayer), typeof(CPosition)));
-            */
+                .All(typeof(CPlayer), typeof(CPlayerCosmetics), typeof(CShoeEffect), typeof(CPosition)));
+            Slowers = GetEntityQuery(new QueryHelper().All(typeof(CPosition), typeof(CSlowPlayer), typeof(CIsOnFire)).None(typeof(CToolInUse)));
         }
 
         protected override void OnUpdate()
         {
+            var players = Players.ToEntityArray(Allocator.TempJob);
+            var slowers = Slowers.ToComponentDataArray<CPosition>(Allocator.Temp);
+            using NativeArray<CPlayerCosmetics> playerComestics = Players.ToComponentDataArray<CPlayerCosmetics>(Allocator.Temp);
+            using NativeArray<CPosition> playerPosition = Players.ToComponentDataArray<CPosition>(Allocator.Temp);
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (playerComestics[i].Shoe == (PlayerShoe)904)
+                {
+                    ComputePlayerSpeed(players[i], playerPosition[i], slowers);
+                }
+            }
+            players.Dispose();
+            slowers.Dispose();
+        }
 
+        private void ComputePlayerSpeed(Entity player, CPosition cPosition, NativeArray<CPosition> slowerPositions)
+        {
+            Vector3 playerVector = cPosition.Position.Rounded();
+            foreach (CPosition slowerPosition in slowerPositions)
+            {
+                Vector3 slowerVector = slowerPosition.Position.Rounded();
+                if (playerVector.x == slowerVector.x && playerVector.z == slowerVector.z)
+                {
+                    EntityManager.SetComponentData(player, new CShoeEffect() { IgnoreMess = true, SpeedModifier = 2f });
+                }
+            }
         }
     }
 }
