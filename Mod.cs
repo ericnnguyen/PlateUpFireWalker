9fecb8f5aa7c448655b0c22dae5027e31d5823ee
Nghia Nguyen <neanguyen@ucdavis.edu>
2023-05-21 20:39:10 -0700

    Set version to 0.1.0

--- Mod.cs
+++ Mod.cs
@@ -21,7 +21,7 @@ namespace KitchenFireWalker
         // Mod Version must follow semver notation e.g. "1.2.3"
         public const string MOD_GUID = "Nghia.PlateUp.FireWalker";
         public const string MOD_NAME = "FireWalker";
-        public const string MOD_VERSION = "0.3.0";
+        public const string MOD_VERSION = "0.1.0";
         public const string MOD_AUTHOR = "Nghia";
         public const string MOD_GAMEVERSION = ">=1.1.4";
         // Game version this mod is designed for in semver
